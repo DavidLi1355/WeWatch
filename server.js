0a8c5f28e7d9f04c2e4cd702efd1aa9c39e546d3
DavidLi1355 <lid1355218215@outlook.com>
2020-12-01 17:22:38 -0500

    created basic ui for main page

--- server.js
+++ server.js
@@ -1,3 +1,21 @@
-const express = require('express')
-const app = express()
-const server = require('http').Server(app)
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+
+const port = process.env.PORT || 3000;
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("main");
+});
+
+app.get("/:room", (req, res) => {
+    res.render("main");
+});
+
+server.listen(port, () => {
+    console.log(`App listening on port ${port}!`);
+});
