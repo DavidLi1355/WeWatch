4af65e487f122618f1b9c7eb6a1554134b276185
DavidLi1355 <lid1355218215@outlook.com>
2020-12-02 12:20:21 -0500

    finished create room function

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const { Socket } = require("socket.io");
 const app = express();
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
+const { v4: uuidV4 } = require("uuid");
 
 const port = process.env.PORT || 3000;
 
@@ -12,8 +14,21 @@ app.get("/", (req, res) => {
     res.render("main");
 });
 
-app.get("/:room", (req, res) => {
-    res.render("main");
+app.get("/api/create", (req, res) => {
+    console.log("hi");
+    res.json({ roomID: `/${uuidV4()}` });
+});
+
+app.get("/:roomID", (req, res) => {
+    res.render("room");
+});
+
+io.on("connection", (socket) => {
+    socket.on("join-room", (roomID, userID) => {
+        socket.join(roomID);
+        socket.to(roomID).broadcast.emit();
+        console.log(roomID, userID);
+    });
 });
 
 server.listen(port, () => {
